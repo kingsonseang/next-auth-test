2baf6b578e16e8a907043658f8e76edf44d12870
kingsonseang <kingsonseang@gmail.com>
2024-08-14 10:04:36 +0100

    fixed missing key prop on ./app/(navigations)/(home)/tracking-rates.tsx

--- app/(navigations)/(home)/tracking-rates.tsx
+++ app/(navigations)/(home)/tracking-rates.tsx
@@ -56,6 +56,7 @@ export function TrackingAndRates() {
                 {Array.from({ length: trackingIdLenght }).map((_, index) => (
                   <Input
                     id={`trackingId[${index}]`}
+                    key={`trackingId[${index}]`}
                     placeholder="Tracking Number"
                   />
                 ))}
